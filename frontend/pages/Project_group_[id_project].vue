b74cd2e72e028f64eabbba1c1e977680359b8de4
danielott <danielgomesott@gmail.com>
2024-07-01 08:45:50 -0300

    pegando id do projeto da url da pagina do projeto

--- frontend/pages/Project_group_[id_project].vue
+++ frontend/pages/Project_group_[id_project].vue
@@ -1,7 +1,11 @@
 <script lang="js" setup>
+import NavBar from '~/components/layoutComponents/NavBar.vue';
+const route = useRoute();
+
+
 
 </script>
 
 <template>
-    
+    <NavBar />
 </template>
